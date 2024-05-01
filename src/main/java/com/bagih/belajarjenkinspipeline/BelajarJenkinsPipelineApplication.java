eba374a422624025f9dc2f4fa498ed5238d461ab
bagih <hartawanbagie@gmail.com>
2024-05-01 19:33:41 +0800

    initial commit

---
+++ src/main/java/com/bagih/belajarjenkinspipeline/BelajarJenkinsPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.bagih.belajarjenkinspipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarJenkinsPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarJenkinsPipelineApplication.class, args);
+	}
+
+}
